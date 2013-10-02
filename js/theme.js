07b7c9af21fad94a3790c9d1ea05f78a3ff37543
Joni Giuro <joni.giuro@gmail.com>
2013-10-02 10:04:23 +0200

    added options

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,18 @@
-var Theme = function() {
+var Theme = function( options ) {
 
-	this.fadeLength = 500;
+	this.fadeLength = options.fadeLength;
 	this.can = null;
 	this.ctx = null;
-	this.v = document.getElementById('video');
+	this.v = options.v;
 	this.vWidth = $(this.v).width();
 	this.vHeight = $(this.v).height();
 	this.smallWidth = this.vWidth / 10;
 	this.smallHeight = this.vHeight / 10;
 	this.totPixels = this.smallWidth * this.smallHeight;
-	this.canvasContainer = $('#canvas-container');
-	this.targetElement = $('#wrapper');
-	this.colorOpacity = .7;
+	this.canvasContainer = options.canvasContainer;
+	this.targetElements = options.targetElements;
+	this.colorOpacity = options.colorOpacity;
+	this.totElements = options.targetElements.length
 
 	this.init = function() {
 		can = document.createElement('canvas');
@@ -82,10 +83,22 @@ var Theme = function() {
 	}
 
 	this.setTheme = function(r, g, b) {
-		this.targetElement.css('background-color','rgba(' + r + ',' + g + ',' + b + ', ' + this.colorOpacity + ')');
+		for (var i = 0 ; i < this.totElements ; i++) {
+			this.targetElements[i].css('background-color','rgba(' + r + ',' + g + ',' + b + ', ' + this.colorOpacity + ')');
+
+			/* JUST FOR DEMO PURPOSE*/
+			$('.value').text('rgba(' + r + ', ' + g + ', ' + b + ', ' + this.colorOpacity + ')');
+		}
+
 	} 
 
 	this.init();
 }
 
-var theme = new Theme();
+var theme = new Theme({
+	fadeLength 		: 500, //each x milliseconds color information will be read from the video
+	v 		   		: document.getElementById('video'), //video element itself
+	canvasContainer : $('#canvas-container'), //where to put the canvas
+	targetElements 	: [$('#wrapper'), $('.current-color')], //what do you want the color to be set to
+	colorOpacity 	: .7 //opacity of the color
+});
